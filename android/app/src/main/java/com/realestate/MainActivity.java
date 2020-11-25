87f40b5d440a304428dc4a5ae3a681baa1af3564
saiashish9 <saiashish3760@gmail.com>
2020-11-26 01:10:55 +0530

    splash screen updated

--- android/app/src/main/java/com/realestate/MainActivity.java
+++ android/app/src/main/java/com/realestate/MainActivity.java
@@ -1,6 +1,7 @@
 package com.realestate;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -8,6 +9,13 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    RNBootSplash.init(R.drawable.bootsplash,MainActivity.this);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "realestate";
